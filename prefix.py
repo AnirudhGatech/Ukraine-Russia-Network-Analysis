8e37b679eb74007b6b591b14063e115e09bb0653
AnirudhGatech <143911409+AnirudhGatech@users.noreply.github.com>
2024-01-23 16:00:29 -0500

    prefix.py

---
+++ prefix.py
@@ -0,0 +1,12 @@
+import json
+
+with open('response_ukraine.txt', 'r') as fp:
+    data = json.load(fp)
+fp.close()
+prefixes = data['data']['resources']['ipv4']
+
+prefixes.extend(data['data']['resources']['ipv6'])
+
+with open('prefixes_ukraine.txt', 'w') as f:
+    for p in prefixes:
+        f.write(f"{p}\n")
