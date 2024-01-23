e9d42cac9754c7c16290c76b037d36fd22c0d19c
AnirudhGatech <143911409+AnirudhGatech@users.noreply.github.com>
2024-01-23 16:00:04 -0500

    Create example.py

---
+++ example.py
@@ -0,0 +1,26 @@
+# import pybgpstream
+# stream = pybgpstream.BGPStream(
+#     from_time="2017-07-07 00:00:00", until_time="2017-07-07 00:10:00 UTC",
+#     collectors=["route-views.sg", "route-views.eqix"],
+#     record_type="updates",
+#     filter="peer 11666 and prefix more 210.180.0.0/16"
+# )
+
+# for elem in stream:
+#     # record fields can be accessed directly from elem
+#     # e.g. elem.time
+#     # or via elem.record
+#     # e.g. elem.record.time
+#     print(elem)
+
+import requests as r
+import json
+
+url = "https://stat.ripe.net/data/country-resource-list/data.json?resource=ua&time=2022-02-22&v4format=prefix"
+
+resp = r.get(url)
+
+
+
+with open('response_ukraine.txt', 'w') as f:
+    json.dump(resp.json(), f, indent=4)
